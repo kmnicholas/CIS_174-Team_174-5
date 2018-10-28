7a6b4014d692e4ef8b555d6cc0f204b24b420a9c
kmnicholas <kmnicholas@dmacc.edu>
2018-10-28 18:49:27 -0500

    first commit

---
+++ RealTimeSportsStatistics/FootballApplication.Domain/Entities/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FootballApplication.Domain.Entities
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string TeamHome { get; set; }
+        public string TeamName { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Ties { get; set; }
+
+    }
+}
---
+++ RealTimeSportsStatistics/RealTimeSportsStatistics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RealTimeSportsStatistics.MvcApplication" Language="C#" %>
